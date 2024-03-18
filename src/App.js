235d0e8b47636f059620d0b8c400c3f5cfdbc523
Jefferson Cargua <jeffersoncargua@gmail.com>
2024-03-18 11:02:06 -0500

    Version 5. SoundShop

--- src/App.js
+++ src/App.js
@@ -5,15 +5,42 @@ import { AllRoutes } from './routes/AllRoutes';
 
 import './App.css';
 
-
 function App() {
 
   const [count,setCount] = useState(0);
+  const [product,setProduct] = useState({});
+  const [shoppingcart,setShoppingCart] = useState([]);
+  const [productList, setProductList] = useState([
+    {
+      id: 10001,
+      name: 'Headphone Sony XM-1101',
+      imagen: '../public/assets/product.jpg',
+      available: 5
+    },
+    {
+      id: 10002,
+      name: 'Headphone Sony PO-1000',
+      imagen: '../public/assets/audifono1.jpg',
+      available: 10
+    },
+    {
+      id: 10003,
+      name: 'Headphone Sony LT-3101',
+      imagen: './public/assets/audifono2.jpg',
+      available: 15
+    },
+    {
+      id: 10004,
+      name: 'Headphone Sony WP-5001',
+      imagen: './public/assets/audifono3.jpg',
+      available: 20
+    }
+  ]);
 
   return (
     <div className="App">
       <Header count={count} setCount={setCount}/>
-      <AllRoutes count={count} setCount={setCount} />
+      <AllRoutes count={count} setCount={setCount} productList={productList} setProductList={setProductList} shoppingcart={shoppingcart} setShoppingCart={setShoppingCart} product={product} setProduct={setProduct} />
       <Footer />
     </div>
   );
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,39 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 //import { useState, useEffect } from "react";
-import { useState } from "react";
 
 import { Home,Cart,Contact,PageNotFound } from "../pages/index";
 
 
-export const AllRoutes = ({count,setCount}) => {
-    const [product,setProduct] = useState({});
-    const [shoppingcart,setShoppingCart] = useState([]);
-    const [productList, setProductList] = useState([
-    {
-      id: 10001,
-      name: 'Headphone Sony XM-1101',
-      imagen: '../public/assets/product.jpg',
-      available: 5
-    },
-    {
-      id: 10002,
-      name: 'Headphone Sony PO-1000',
-      imagen: '../public/assets/audifono1.jpg',
-      available: 10
-    },
-    {
-      id: 10003,
-      name: 'Headphone Sony LT-3101',
-      imagen: './public/assets/audifono2.jpg',
-      available: 15
-    },
-    {
-      id: 10004,
-      name: 'Headphone Sony WP-5001',
-      imagen: './public/assets/audifono3.jpg',
-      available: 20
-    }
-  ]);
+export const AllRoutes = ({count,setCount,productList,setProductList,shoppingcart,setShoppingCart,product,setProduct}) => {
 
  /*useEffect(() => {
   setProductList(productList);
